e57cca98eac3b17c9cdc54183f8ec94c17a88f26
0xNino <86327707+0xNino@users.noreply.github.com>
2022-02-04 16:53:41 +0100

    init files

---
+++ include/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 16:47:14 by 0xNino            #+#    #+#             */
+/*   Updated: 2022/02/04 16:48:46 by 0xNino           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include "../libft/libft.h"
+
+#endif
