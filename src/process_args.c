ffed3db9d5c11150dccf63edc4dafb3d3a25e6aa
0xNino <86327707+0xNino@users.noreply.github.com>
2022-02-13 18:10:00 +0100

    start args processing

--- src/process_args.c
+++ src/process_args.c
@@ -6,14 +6,32 @@
 /*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 00:23:13 by 0xNino            #+#    #+#             */
-/*   Updated: 2022/02/12 00:38:20 by 0xNino           ###   ########.fr       */
+/*   Updated: 2022/02/13 18:06:50 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-t_dlist	str_to_dlst(char *str)
+t_dlist	*str_to_dlst(char *str)
 {
+	t_dlist	*dlst;
+	int		i;
+	int		*content;
+
+	dlst = ft_dlstnew();
+	i = 0;
+	while (str + i)
+	{
+		if (ft_isnumber(str + i))
+		{
+			content = malloc(sizeof(int));
+			if (!content)
+				return (NULL);
+			content = ft_atoi(str + i);
+			dlst = ft_dlstadd_last(dlst, content);
+		}
+	}
+	return (dlst);
 }
 
 t_dlist	arr_to_dlst(int argc, char **argv)
