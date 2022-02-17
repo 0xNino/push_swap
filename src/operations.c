8296ba7d40f5635d1c096a9d5abc082b58a3fac7
0xNino <86327707+0xNino@users.noreply.github.com>
2022-02-17 23:27:35 +0100

    start solve_5

--- src/operations.c
+++ src/operations.c
@@ -6,7 +6,7 @@
 /*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 15:33:52 by 0xNino            #+#    #+#             */
-/*   Updated: 2022/02/17 20:56:32 by 0xNino           ###   ########.fr       */
+/*   Updated: 2022/02/17 21:02:52 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,16 @@ void	operations(t_stacks *stacks, char *operation)
 	if (!ft_strcmp(operation, "sb"))
 		swap(stacks->b);
 	if (!ft_strcmp(operation, "pa"))
-		ft_dlstputfirst(stacks->a, ft_dlstpushfirst(stacks->b));
+		ft_dlstput_first(stacks->a, ft_dlstpush_first(stacks->b));
 	if (!ft_strcmp(operation, "pb"))
-		ft_dlstputfirst(stacks->b, ft_dlstpushfirst(stacks->a));
+		ft_dlstput_first(stacks->b, ft_dlstpush_first(stacks->a));
 	if (!ft_strcmp(operation, "ra") || !ft_strcmp(operation, "rr"))
-		ft_dlstputlast(stacks->a, ft_dlstpushfirst(stacks->a));
+		ft_dlstput_last(stacks->a, ft_dlstpush_first(stacks->a));
 	if (!ft_strcmp(operation, "rb") || !ft_strcmp(operation, "rr"))
-		ft_dlstputlast(stacks->b, ft_dlstpushfirst(stacks->b));
+		ft_dlstput_last(stacks->b, ft_dlstpush_first(stacks->b));
 	if (!ft_strcmp(operation, "rra") || !ft_strcmp(operation, "rrr"))
-		ft_dlstputfirst(stacks->a, ft_dlstpushlast(stacks->a));
+		ft_dlstput_first(stacks->a, ft_dlstpush_last(stacks->a));
 	if (!ft_strcmp(operation, "rrb") || !ft_strcmp(operation, "rrr"))
-		ft_dlstputfirst(stacks->b, ft_dlstpushlast(stacks->b));
+		ft_dlstput_first(stacks->b, ft_dlstpush_last(stacks->b));
 	ft_dlstadd_last(stacks->solution, ft_strdup(operation));
 }
--- src/solve.c
+++ src/solve.c
@@ -6,7 +6,7 @@
 /*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 17:06:40 by 0xNino            #+#    #+#             */
-/*   Updated: 2022/02/17 17:59:03 by 0xNino           ###   ########.fr       */
+/*   Updated: 2022/02/17 21:34:46 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,15 @@ void	solve_3(t_stacks *stacks)
 
 void	solve_5(t_stacks *stacks)
 {
-	if (stacks)
-		return ;
+	operations(stacks, "pb");
+	operations(stacks, "pb");
+	solve_3(stacks);
+	operations(stacks, "pa");
+	if (node_isbigger(stacks->a->first, stacks->a->first->next))
+		operations(stacks, "ra");
+	operations(stacks, "pa");
+	if (node_isbigger(stacks->a->first, stacks->a->first->next))
+		operations(stacks, "ra");
 }
 
 void	solve_big(t_stacks *stacks)
